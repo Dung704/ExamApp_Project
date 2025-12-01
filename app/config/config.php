5116956b9c952621651cb96ecd2ce575c3c8d1c1
Ren2908 <phambaduong2809@gmail.com>
2025-12-01 11:00:24 +0700

    cập nhật tên db

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,10 @@
 <?php
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_HOST','localhost');
-define('DB_NAME','');
+define('DB_USER', 'root');           // user MySQL
+define('DB_PASSWORD', '');           // password MySQL
+define('DB_HOST', 'localhost');      // host
+define('DB_NAME', 'quanlydethitracnghiem'); // tên database đã tạo
 
-$dbc = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
-        OR die ('Không kết nối được với MySQL:' . mysqli_connect_error());
+$dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+        or die('Không kết nối được với MySQL: ' . mysqli_connect_error());
 
-mysqli_set_charset($dbc,'utf8');
-
-?>
+mysqli_set_charset($dbc, 'utf8mb4');
